b29cd160e9baf89a2d0216c8c063d582c6a8b711
Fabien Zucchet <fabien.zucchet@viarezo.fr>
2021-05-22 18:07:00 +0200

    feat(dice): allow sending random or specific dice

--- messages.go
+++ messages.go
@@ -3,6 +3,7 @@ package telebot
 import (
 	"encoding/json"
 	"log"
+	"math/rand"
 	"net/url"
 	"strconv"
 )
@@ -216,12 +217,34 @@ func (b *Bot) DeleteMessage(chatId int, messageId int) (string, error) {
 
 }
 
-// Send a dice
+// send a dice
 func (b *Bot) SendDice(chatId int, options SendMessageOptions) (string, error) {
+
+	return b.SendDiceEmoji(chatId, "", options)
+}
+
+// send a random dice
+func (b *Bot) SendRandomDice(chatId int, options SendMessageOptions) (string, error) {
+
+	emojiList := []string{"🎲", "🎯", "🏀", "⚽", "🎳", "🎰"}
+
+	emoji := emojiList[rand.Intn(len(emojiList))]
+
+	return b.SendDiceEmoji(chatId, emoji, options)
+
+}
+
+// Send a dice Emoji (Supported emojis : “🎲”, “🎯”, “🏀”, “⚽”, “🎳”, or “🎰”. Default is “🎲”. )
+func (b *Bot) SendDiceEmoji(chatId int, emoji string, options SendMessageOptions) (string, error) {
 	val := url.Values{
 		"chat_id":                     {strconv.Itoa(chatId)},
 		"disable_notification":        {strconv.FormatBool(options.DisableNotification)},
 		"allow_sending_without_reply": {strconv.FormatBool(options.AllowSendingWithoutReply)},
+	}
+
+	// Emoji (Supported emojis : “🎲”, “🎯”, “🏀”, “⚽”, “🎳”, or “🎰”. Default is “🎲”. )
+	if emoji != "" {
+		val["emoji"] = []string{emoji}
 	}
 
 	// Reply to message
